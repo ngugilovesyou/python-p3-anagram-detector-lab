fea2557ecbb50112219c7098823b68551b140419
samuelgitau <amsamgitau@gmail.com>
2024-08-30 15:21:29 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, names) -> None:
+        self._name = sorted(names)
+    def match(self, words):
+        word_list = []
+        
+        for word in words:
+            if sorted(word) == self._name:
+                word_list.append(word)
+
+        return word_list
